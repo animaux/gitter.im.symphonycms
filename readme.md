62a461d3190e3f50a0797508629ee50d90eeb12f
Alexander Rutz <ar@animaux.de>
2017-02-28 16:40:03 +0100

    markdown files for better human-readability from within github

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Chat archive of https://gitter.im/symphonycms/symphony-2 dating from `2015-04-01` to `2017-02-28`.
+
+Use repository search to find what you are looking for. 
--- suckitdown.sh
+++ suckitdown.sh
@@ -1,5 +1,8 @@
 # extracted from https://gitter.im/symphonycms/symphony-2/archives/
 #
+# converted to md html2text w/ github.com/Alir3z4/html2text
+#
+# find ./ -iname "*.html" -type f -exec sh -c 'html2text "${0}" > "../md/${0%.html}.md"' {} \;
 #
 # CURRENTLY MOST RECENT ITEM
 # curl -o "2017-02-01.html" "https://gitter.im/symphonycms/symphony-2/archives/2017/02/01"
