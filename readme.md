fb7b870aca205d9ababebe658941ab3fafc5c455
Alexander Rutz <ar@animaux.de>
2018-09-24 10:13:45 +0200

    Update 2018-09-24

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-Chat archive of https://gitter.im/symphonycms/symphony-2 dating from `2015-03-27` to `2017-02-28`.
+Chat archive of https://gitter.im/symphonycms/symphony-2 dating from `2015/03/27` to `2018/09/24`.
 
 ### Usage
 
-Use repository search to find what you are looking for, then choose `Languages —> Markdown` for better human readability.
+Use *repository search* to find what you are looking for, then choose `Languages —> Markdown` for better human readability.
--- suckitdown.sh
+++ suckitdown.sh
@@ -1,9 +1,11 @@
-# extracted from https://gitter.im/symphonycms/symphony-2/archives/
-#
-# converted to md html2text w/ github.com/Alir3z4/html2text
-#
-# find ./ -iname "*.html" -type f -exec sh -c 'html2text "${0}" > "../md/${0%.html}.md"' {} \;
-#
-# CURRENTLY MOST RECENT ITEM
-# curl -o "2017-02-01.html" "https://gitter.im/symphonycms/symphony-2/archives/2017/02/01"
+# extract Links from https://gitter.im/symphonycms/symphony-2/archives/ HTML Document
+sed -n 's/.*href="\([^"]*\).*/\1/p' gitter.html > gitter.sh
 
+# BBEDit, cleanup and Search/Replace:
+# Find: (/symphonycms/symphony-2/archives)/(\d\d\d\d)/(\d\d)/(\d\d)
+# Replace: curl -o "\2-\3-\4.html" "https://gitter.im\1/\2/\3/\4"
+
+# CURRENTLY MOST RECENT ITEM /2018/09/24
+
+# converted to md html2text w/ github.com/Alir3z4/html2text –> pip install html2text
+find ./ -iname "*.html" -type f -exec sh -c 'html2text "${0}" > "../md/${0%.html}.md"' {} \;
